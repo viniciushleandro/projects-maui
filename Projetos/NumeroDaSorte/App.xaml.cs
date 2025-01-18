5784d75da4363e0129b720c5bb93a1732e65bd83
Vinicius <vinilean43@gmail.com>
2025-01-18 13:43:54 -0300

    Primeiro commit

---
+++ Projetos/NumeroDaSorte/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NumeroDaSorte
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Projetos/NumeroDaSorte/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NumeroDaSorte
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
